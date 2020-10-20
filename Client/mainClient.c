087a6d5190cc292205b7f4b6af7e6880dcbdd0ae
JUANMA <juanmafisolis242@gmail.com>
2020-10-19 23:12:58 -0600

    Sockets json
    ....

--- Client/mainClient.c
+++ Client/mainClient.c
@@ -1,64 +1,209 @@
 /*
 	C ECHO client example using sockets
 */
-#include <stdio.h>	//printf
-#include <string.h>	//strlen
-#include <sys/socket.h>	//socket
-#include <arpa/inet.h>	//inet_addr
+#include <stdio.h>       //printf
+#include <string.h>      //strlen
+#include <sys/socket.h>  //socket
+#include <arpa/inet.h>   //inet_addr
+#include <json-c/json.h> //JSON
 #include <unistd.h>
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-	int sock;
-	struct sockaddr_in server;
-	char message[1000] , server_reply[2000];
-	
-	//Create socket
-	sock = socket(AF_INET , SOCK_STREAM , 0);
-	if (sock == -1)
-	{
-		printf("Could not create socket");
-	}
-	puts("Socket created");
-	
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_family = AF_INET;
-	server.sin_port = htons( 8888 );
+    int sock;
+    struct sockaddr_in server;
+    char message[1000], server_reply[2000];
 
-	//Connect to remote server
-	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		perror("connect failed. Error");
-		return 1;
-	}
-	
-	
-	puts("Connected\n");
-	
-	//keep communicating with server
-	while(1)
-	{
-		printf("Enter message : ");
-		scanf("%s" , message);
-		
-		//Send some data
-		if( send(sock , message , strlen(message) , 0) < 0)
-		{
-			puts("Send failed");
-			return 1;
-		}
-		
-		//Receive a reply from the server
-		if( recv(sock , server_reply , 2000 , 0) < 0)
-		{
-			puts("recv failed");
-			break;
-		}
-		
-		puts("Server reply :");
-		puts(server_reply);
-	}
-	
-	close(sock);
-	return 0;
+    //Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
+
+    //Connect to remote server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    //keep communicating with server
+    int flag = 0;
+    while (1)
+    {
+        if (flag == 1)
+        {
+            printf("Enter message : ");
+            scanf("%s", message);
+
+            // //Send some data
+            // if (send(sock, message, strlen(message), 0) < 0)
+            // {
+            //     puts("Send failed");
+            //     return 1;
+            // }
+
+            //Receive a reply from the server
+            if (recv(sock, server_reply, 2000, 0) < 0)
+            {
+                puts("recv failed");
+                break;
+            }
+
+            puts("EL SERVER ENVIO :");
+            puts(server_reply);
+            memset(server_reply, 0, sizeof(server_reply));
+
+            ///////////////////////////////////////
+            ///////////////////////////////////////
+            /*Creating a json object*/
+            json_object *jobj = json_object_new_object();
+
+            /*Creating a json string*/
+            json_object *jstring = json_object_new_string("PONCHO");
+
+            /*Creating a json integer*/
+            json_object *jint = json_object_new_int(15);
+
+            // /*Creating a json boolean*/
+            // json_object *jboolean = json_object_new_boolean(1);
+
+            // /*Creating a json double*/
+            // json_object *jdouble = json_object_new_double(2.14);
+
+            /*Creating a json array*/
+            json_object *jarray = json_object_new_array();
+
+            /*Creating json strings*/
+            json_object *jstring1 = json_object_new_string("PAULA");
+            json_object *jstring2 = json_object_new_string("PAULAA");
+            json_object *jstring3 = json_object_new_string("Y PAULAAA");
+
+            /*Adding the above created json strings to the array*/
+            json_object_array_add(jarray, jstring1);
+            json_object_array_add(jarray, jstring2);
+            json_object_array_add(jarray, jstring3);
+
+            /*Form the json object*/
+            /*Each of these is like a key value pair*/
+            json_object_object_add(jobj, "name", jstring);
+            // json_object_object_add(jobj, "Technical blog", jboolean);
+            // json_object_object_add(jobj, "Average posts per day", jdouble);
+            json_object_object_add(jobj, "age", jint);
+            json_object_object_add(jobj, "friends", jarray);
+
+            printf("Size of JSON object- %lu\n", sizeof(jobj));
+            printf("Size of JSON_TO_STRING- %lu,\n %s\n", sizeof(json_object_to_json_string(jobj)), json_object_to_json_string(jobj));
+            char temp_buff[2000];
+
+            if (strcpy(temp_buff, json_object_to_json_string(jobj)) == NULL)
+            {
+                perror("strcpy");
+                return EXIT_FAILURE;
+            }
+
+            //ACA LO ENVIA
+            //send(sock, message, strlen(message), 0) < 0
+            if (send(sock, temp_buff, strlen(temp_buff),0) < 0 )
+            {
+                perror("demodemoserverAddrserverAddr");
+                return EXIT_FAILURE;
+            }
+            /////////////////////////////////////////////
+            /////////////////////////////////////////////
+            /////////////////////////////////////////////
+        }
+        else
+        {
+            // memset(message, 0, sizeof(message));
+
+            // //Send some data
+            // if (send(sock, message, strlen(message), 0) < 0)
+            // {
+            //     puts("Send failed");
+            //     return 1;
+            // }
+            ///////////////////////////////////////
+            ///////////////////////////////////////
+            /*Creating a json object*/
+            json_object *jobj = json_object_new_object();
+
+            /*Creating a json string*/
+            json_object *jstring = json_object_new_string("PONCHO");
+
+            /*Creating a json integer*/
+            json_object *jint = json_object_new_int(15);
+
+            // /*Creating a json boolean*/
+            // json_object *jboolean = json_object_new_boolean(1);
+
+            // /*Creating a json double*/
+            // json_object *jdouble = json_object_new_double(2.14);
+
+            /*Creating a json array*/
+            json_object *jarray = json_object_new_array();
+
+            /*Creating json strings*/
+            json_object *jstring1 = json_object_new_string("PAULA");
+            json_object *jstring2 = json_object_new_string("PAULAA");
+            json_object *jstring3 = json_object_new_string("Y PAULAAA");
+
+            /*Adding the above created json strings to the array*/
+            json_object_array_add(jarray, jstring1);
+            json_object_array_add(jarray, jstring2);
+            json_object_array_add(jarray, jstring3);
+
+            /*Form the json object*/
+            /*Each of these is like a key value pair*/
+            json_object_object_add(jobj, "name", jstring);
+            // json_object_object_add(jobj, "Technical blog", jboolean);
+            // json_object_object_add(jobj, "Average posts per day", jdouble);
+            json_object_object_add(jobj, "age", jint);
+            json_object_object_add(jobj, "friends", jarray);
+
+            printf("Size of JSON object- %lu\n", sizeof(jobj));
+            printf("Size of JSON_TO_STRING- %lu,\n %s\n", sizeof(json_object_to_json_string(jobj)), json_object_to_json_string(jobj));
+            char temp_buff[2000];
+
+            if (strcpy(temp_buff, json_object_to_json_string(jobj)) == NULL)
+            {
+                perror("strcpy");
+                return EXIT_FAILURE;
+            }
+
+            //ACA LO ENVIA
+            //send(sock, message, strlen(message), 0) < 0
+            if (send(sock, temp_buff, strlen(temp_buff),0) < 0 )
+            {
+                perror("demodemoserverAddrserverAddr");
+                return EXIT_FAILURE;
+            }
+            /////////////////////////////////////////////
+            /////////////////////////////////////////////
+            /////////////////////////////////////////////
+
+            //Receive a reply from the server
+            if (recv(sock, server_reply, 2000, 0) < 0)
+            {
+                puts("recv failed");
+                break;
+            }
+
+            puts("Server reply :");
+            puts(server_reply);
+            memset(server_reply, 0, sizeof(server_reply));
+            flag++;
+        }
+    }
+
+    close(sock);
+    return 0;
 }
--- Server/mainServer.c
+++ Server/mainServer.c
@@ -1,79 +1,83 @@
+/*
+	C socket server example, handles multiple clients using threads
+*/
 
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<stdlib.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
-#include<pthread.h> //for threading , link with lpthread
+#include <stdio.h>
+#include <string.h> //strlen
+#include <stdlib.h> //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h>   //inet_addr
+#include <unistd.h>      //write
+#include <pthread.h>     //for threading , link with lpthread
+#include <json-c/json.h> //JSON
 
 //the thread function
 void *connection_handler(void *);
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
-	int socket_desc , client_sock , c , *new_sock;
-	struct sockaddr_in server , client;
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
-	{
-		printf("Could not create socket");
-	}
-	puts("Socket created");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
-	
-	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		//print the error message
-		perror("bind failed. Error");
-		return 1;
-	}
-	puts("bind done");
-	
-	//Listen
-	listen(socket_desc , 3);
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-	{
-		puts("Connection accepted");
-		
-		pthread_t sniffer_thread;
-		new_sock = malloc(1);
-		*new_sock = client_sock;
-		
-		if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-		{
-			perror("could not create thread");
-			return 1;
-		}
-		
-		//Now join the thread , so that we dont terminate before the thread
-		//pthread_join( sniffer_thread , NULL);
-		puts("Handler assigned");
-	}
-	
-	if (client_sock < 0)
-	{
-		perror("accept failed");
-		return 1;
-	}
-	
-	return 0;
+
+    int socket_desc, client_sock, c, *new_sock;
+    struct sockaddr_in server, client;
+
+    //Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    //Bind
+    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc, 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    while ((client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c)))
+    {
+        puts("Connection accepted");
+
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+
+        if (pthread_create(&sniffer_thread, NULL, connection_handler, (void *)new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( sniffer_thread , NULL);
+        puts("Handler assigned");
+    }
+
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+
+    return 0;
 }
 
 /*
@@ -81,37 +85,70 @@ int main(int argc , char *argv[])
  * */
 void *connection_handler(void *socket_desc)
 {
-	//Get the socket descriptor
-	int sock = *(int*)socket_desc;
-	int read_size;
-	char *message , client_message[2000];
-	
-	//Send some messages to the client
-	message = "Greetings! I am your connection handler\n";
-	write(sock , message , strlen(message));
-	
-	message = "Now type something and i shall repeat what you type \n";
-	write(sock , message , strlen(message));
-	
-	//Receive a message from client
-	while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-	{
-		//Send the message back to client
-		write(sock , client_message , strlen(client_message));
-	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
-		
-	//Free the socket pointer
-	free(socket_desc);
-	
-	return 0;
+    struct json_object *parsed_json;
+    struct json_object *name;
+    struct json_object *age;
+    struct json_object *friends;
+    struct json_object *friend;
+    size_t n_friends;
+
+    size_t i;
+    //Get the socket descriptor
+    int sock = *(int *)socket_desc;
+    int read_size;
+    char *message;
+    char client_message[2000];
+
+    //Send some messages to the client
+    message = " Greetings! I am your connection handler\n Now type something and i shall repeat what you type \n";
+    write(sock, message, strlen(message));
+
+    // message = "Now type something and i shall repeat what you type \n";
+    // write(sock , message , strlen(message));
+
+    //Receive a message from client
+
+    while ((read_size = recv(sock, client_message, 2000, 0)) > 0)
+    {
+        
+
+        //if(IsJsonString(client_message)
+        parsed_json = json_tokener_parse(client_message);
+        json_object_object_get_ex(parsed_json, "name", &name);
+        json_object_object_get_ex(parsed_json, "age", &age);
+        json_object_object_get_ex(parsed_json, "friends", &friends);
+
+        printf("Name: %s\n", json_object_get_string(name));
+        printf("Age: %d\n", json_object_get_int(age));
+
+        n_friends = json_object_array_length(friends);
+        printf("Found %lu friends\n", n_friends);
+
+        for (i = 0; i < n_friends; i++)
+        {
+            friend = json_object_array_get_idx(friends, i);
+            printf("%lu. %s\n", i + 1, json_object_get_string(friend));
+        }
+
+        //Send the message back to client
+        puts("EL CLIENTE ENVIO :");
+        puts(client_message);
+        write(sock, client_message, strlen(client_message));
+        memset(client_message, 0, sizeof(client_message));
+    }
+
+    if (read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if (read_size == -1)
+    {
+        perror("recv failed");
+    }
+
+    //Free the socket pointer
+    free(socket_desc);
+
+    return 0;
 }
