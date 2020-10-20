ea6b98518a87d554d552ad552e0d23a13c29612a
JUANMA <juanmafisolis242@gmail.com>
2020-10-20 06:17:09 -0600

    Cliente-lite

--- Client/mainClient.c
+++ Client/mainClient.c
@@ -18,21 +18,22 @@
 
 void *foo(void *p)
 {
-
     int *my_data = p; /* data received by thread */
-    int k = my_data[0];
     printf("\n");
     printf("SOY EL HILO LOS DATOS DEL PROCESO SON : \n");
-    printf("EL BURST DEL PROCESO ES DE : %d\n", k);
+    printf("EL BURST DEL PROCESO ES DE : %d\n", my_data[0]);
     printf("LA PRIOIRDAD DEL PROCESO ES DE : %d\n", my_data[1]);
     // Return reference to global variable:
     pthread_exit(&my_data);
 }
 
+
+
+
+
 /////////////////////////////////////////////////////
 // LEE EL ARCHIVO Y GUARDA LOS DATOS EN UN ARREGLO //
 /////////////////////////////////////////////////////
-
 int *readFile(char *pData)
 {
     FILE *fp;
@@ -83,10 +84,13 @@ int *readFile(char *pData)
     return save;
 }
 
+
+
+
+
 /////////////////////////////////////////////////////
 // LEE EL ARCHIVO Y GUARDA LOS DATOS EN UN ARREGLO //
 /////////////////////////////////////////////////////
-
 json_object *makeJson(int pData1, int pData2)
 {
     /*Creating a json object*/
@@ -102,6 +106,11 @@ json_object *makeJson(int pData1, int pData2)
     return jobj;
 }
 
+
+
+/////////////////////////////////////////////////////
+//                     CLIENTE                     //
+/////////////////////////////////////////////////////
 void client()
 {
     pthread_t tid1;
@@ -287,265 +296,6 @@ void client()
 
 int main(int argc, char *argv[])
 {
-
     client();
-    // int sock;
-    // int flag = 0;
-    // int flagComunication = 0;
-    // struct sockaddr_in server;
-    // char message[1000], server_reply[2000];
-
-    // //Create socket
-    // sock = socket(AF_INET, SOCK_STREAM, 0);
-    // if (sock == -1)
-    // {
-    //     printf("Could not create socket");
-    // }
-    // puts("Socket created");
-
-    // server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    // server.sin_family = AF_INET;
-    // server.sin_port = htons(8888);
-
-    // //Connect to remote server
-    // if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
-    // {
-    //     perror("connect failed. Error");
-    //     return 1;
-    // }
-
-    // puts("Connected\n");
-
-    // //keep communicating with server
-
-    // while (flagComunication == 0)
-    // {
-    //     // CONSTRUCCION DEL MENU
-    //     char titulo[] = "\n ||---------------MENU-CLIENTE---------------||";
-    //     char tituloMM[] = "\n ||----------------MODO-MANUAL---------------||";
-    //     char tituloMA[] = "\n ||-------------MODO-AUTOMATICO--------------||";
-    //     char makeRandom[] = "\n ||--------GENERANDO-LOS-DATOS-RANDOMS-------||";
-    //     char searchFile[] = "\n ||------INTRODUZCA-EL-NOMBRE-DEL-ARCHIVO----||";
-    //     char MM[] = "\n || 1. MODO MANUAL                           ||";
-    //     char MA[] = "\n || 2. MODO AUTOMATICO                       ||";
-    //     char exit[] = "\n || 3. SALIR                                 ||";
-    //     char separador[] = "\n ||==========================================||";
-    //     char optionN[] = "\n ||------INTRODUZCA-UNA-OPCION-(1-3)---------||";
-    //     int n, opcion;
-    //     char name[20];
-    //     do
-    //     {
-    //         //MENU PRINCIPAL
-    //         printf(separador, sizeof(separador));
-    //         printf(titulo, sizeof(titulo));
-    //         printf(separador, sizeof(separador));
-    //         printf(MM, sizeof(MM));
-    //         printf(MA, sizeof(MA));
-    //         printf(exit, sizeof(exit));
-    //         printf(separador, sizeof(separador));
-    //         printf(optionN, sizeof(optionN));
-    //         printf(separador, sizeof(separador));
-    //         printf("\n");
-    //         scanf("%d", &opcion);
-
-    //         switch (opcion)
-    //         {
-
-    //         //MENU DE MODO MANUAL
-    //         case 1:
-    //             printf(separador, sizeof(separador));
-    //             printf(tituloMM, sizeof(tituloMM));
-    //             printf(separador, sizeof(separador));
-    //             printf(makeRandom, sizeof(makeRandom));
-    //             printf(separador, sizeof(separador));
-    //             scanf("%d", &n);
-    //             break;
-    //         //MENU DE MODO AUTOMATICO
-    //         case 2:
-    //             printf(separador, sizeof(separador));
-    //             printf(tituloMA, sizeof(tituloMA));
-    //             printf(separador, sizeof(separador));
-    //             printf(searchFile, sizeof(searchFile));
-    //             printf(separador, sizeof(separador));
-    //             printf("\n");
-    //             scanf("%s", &name);
-    //             //OBTIENE LOS DATOS PARA CARGAR EN EL HILO
-    //             readFile(name);
-    //             break;
-
-    //         case 3:
-    //             flagComunication++; //ROMPE AMBOS WHILES Y CIERRA EL SOCKET
-    //             break;
-    //         }
-    //     } while (flagComunication == 0);
-
-    //     /* Fin del anidamiento */
-
-    //     if (flag == 1)
-    //     {
-    //         printf("Enter message : ");
-    //         scanf("%s", message);
-
-    //         // //Send some data
-    //         // if (send(sock, message, strlen(message), 0) < 0)
-    //         // {
-    //         //     puts("Send failed");
-    //         //     return 1;
-    //         // }
-
-    //         //Receive a reply from the server
-    //         if (recv(sock, server_reply, 2000, 0) < 0)
-    //         {
-    //             puts("recv failed");
-    //             break;
-    //         }
-
-    //         puts("EL SERVER ENVIO :");
-    //         puts(server_reply);
-    //         memset(server_reply, 0, sizeof(server_reply));
-
-    //         ///////////////////////////////////////
-    //         ///////////////////////////////////////
-    //         /*Creating a json object*/
-    //         json_object *jobj = json_object_new_object();
-
-    //         /*Creating a json string*/
-    //         json_object *jstring = json_object_new_string("PONCHO");
-
-    //         /*Creating a json integer*/
-    //         json_object *jint = json_object_new_int(15);
-
-    //         // /*Creating a json boolean*/
-    //         // json_object *jboolean = json_object_new_boolean(1);
-
-    //         // /*Creating a json double*/
-    //         // json_object *jdouble = json_object_new_double(2.14);
-
-    //         /*Creating a json array*/
-    //         json_object *jarray = json_object_new_array();
-
-    //         /*Creating json strings*/
-    //         json_object *jstring1 = json_object_new_string("PAULA");
-    //         json_object *jstring2 = json_object_new_string("PAULAA");
-    //         json_object *jstring3 = json_object_new_string("Y PAULAAA");
-
-    //         /*Adding the above created json strings to the array*/
-    //         json_object_array_add(jarray, jstring1);
-    //         json_object_array_add(jarray, jstring2);
-    //         json_object_array_add(jarray, jstring3);
-
-    //         /*Form the json object*/
-    //         /*Each of these is like a key value pair*/
-    //         json_object_object_add(jobj, "name", jstring);
-    //         // json_object_object_add(jobj, "Technical blog", jboolean);
-    //         // json_object_object_add(jobj, "Average posts per day", jdouble);
-    //         json_object_object_add(jobj, "age", jint);
-    //         json_object_object_add(jobj, "friends", jarray);
-
-    //         printf("Size of JSON object- %lu\n", sizeof(jobj));
-    //         printf("Size of JSON_TO_STRING- %lu,\n %s\n", sizeof(json_object_to_json_string(jobj)), json_object_to_json_string(jobj));
-    //         char temp_buff[2000];
-
-    //         if (strcpy(temp_buff, json_object_to_json_string(jobj)) == NULL)
-    //         {
-    //             perror("strcpy");
-    //             return EXIT_FAILURE;
-    //         }
-
-    //         //ACA LO ENVIA
-    //         //send(sock, message, strlen(message), 0) < 0
-    //         if (send(sock, temp_buff, strlen(temp_buff), 0) < 0)
-    //         {
-    //             perror("demodemoserverAddrserverAddr");
-    //             return EXIT_FAILURE;
-    //         }
-    //         /////////////////////////////////////////////
-    //         /////////////////////////////////////////////
-    //         /////////////////////////////////////////////
-    //     }
-    //     else
-    //     {
-    //         // memset(message, 0, sizeof(message));
-
-    //         // //Send some data
-    //         // if (send(sock, message, strlen(message), 0) < 0)
-    //         // {
-    //         //     puts("Send failed");
-    //         //     return 1;
-    //         // }
-    //         ///////////////////////////////////////
-    //         ///////////////////////////////////////
-    //         /*Creating a json object*/
-    //         json_object *jobj = json_object_new_object();
-
-    //         /*Creating a json string*/
-    //         json_object *jstring = json_object_new_string("PONCHO");
-
-    //         /*Creating a json integer*/
-    //         json_object *jint = json_object_new_int(15);
-
-    //         // /*Creating a json boolean*/
-    //         // json_object *jboolean = json_object_new_boolean(1);
-
-    //         // /*Creating a json double*/
-    //         // json_object *jdouble = json_object_new_double(2.14);
-
-    //         /*Creating a json array*/
-    //         json_object *jarray = json_object_new_array();
-
-    //         /*Creating json strings*/
-    //         json_object *jstring1 = json_object_new_string("PAULA");
-    //         json_object *jstring2 = json_object_new_string("PAULAA");
-    //         json_object *jstring3 = json_object_new_string("Y PAULAAA");
-
-    //         /*Adding the above created json strings to the array*/
-    //         json_object_array_add(jarray, jstring1);
-    //         json_object_array_add(jarray, jstring2);
-    //         json_object_array_add(jarray, jstring3);
-
-    //         /*Form the json object*/
-    //         /*Each of these is like a key value pair*/
-    //         json_object_object_add(jobj, "name", jstring);
-    //         // json_object_object_add(jobj, "Technical blog", jboolean);
-    //         // json_object_object_add(jobj, "Average posts per day", jdouble);
-    //         json_object_object_add(jobj, "age", jint);
-    //         json_object_object_add(jobj, "friends", jarray);
-
-    //         printf("Size of JSON object- %lu\n", sizeof(jobj));
-    //         printf("Size of JSON_TO_STRING- %lu,\n %s\n", sizeof(json_object_to_json_string(jobj)), json_object_to_json_string(jobj));
-    //         char temp_buff[2000];
-
-    //         if (strcpy(temp_buff, json_object_to_json_string(jobj)) == NULL)
-    //         {
-    //             perror("strcpy");
-    //             return EXIT_FAILURE;
-    //         }
-
-    //         //ACA LO ENVIA
-    //         //send(sock, message, strlen(message), 0) < 0
-    //         if (send(sock, temp_buff, strlen(temp_buff), 0) < 0)
-    //         {
-    //             perror("demodemoserverAddrserverAddr");
-    //             return EXIT_FAILURE;
-    //         }
-    //         /////////////////////////////////////////////
-    //         /////////////////////////////////////////////
-    //         /////////////////////////////////////////////
-
-    //         //Receive a reply from the server
-    //         if (recv(sock, server_reply, 2000, 0) < 0)
-    //         {
-    //             puts("recv failed");
-    //             break;
-    //         }
-
-    //         puts("Server reply :");
-    //         puts(server_reply);
-    //         memset(server_reply, 0, sizeof(server_reply));
-    //         flag++;
-    //     }
-    // }
-
-    // close(sock);
-    // return 0;
+    return 0;
 }
