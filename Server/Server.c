b47ab5b0c74c913510bc8f7fd74ca529adbfe208
Alfonso Badilla <alfonsog.badilla@gmail.com>
2020-10-23 16:47:26 -0600

    Server Rework

--- Server/Server.c
+++ Server/Server.c
@@ -3,8 +3,104 @@
 */
 #include <stdio.h>
 #include "connection.c"
+#define clrscr() printf("\e[1;1H\e[2J")
 
-int main(){
-    connection();
+pthread_t menuThread;
+
+void *menuServer(void *unused)
+{
+    int opcion;
+    int wState = 1;
+    clrscr();
+    while (wState)
+    {
+        printf("||==========================================||\n");
+        printf("||----------------MENU-SERVER---------------||\n");
+        printf("||==========================================||\n");
+        printf("||-------------SELECCIONE-OPCION------------||\n");
+        printf("|| 1. VER JOBTASKLIST                       ||\n");
+        printf("|| 2. DETENER SIMULACION                    ||\n");
+        printf("|| 3. SALIR                                 ||\n");
+        printf("||==========================================||\n");
+        printf("||--------INTRODUZCA-UNA-OPCION-(1-3)-------||\n");
+        printf("||Opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            /* code */
+            clrscr();
+            connectionPrintJobTaskList();
+            // break;
+
+        case 2:
+            /* code */
+            break;
+
+        case 3:
+            /* code */
+            wState = 0;
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int opcion;
+    printf("||==========================================||\n");
+    printf("||----------------MENU-SERVER---------------||\n");
+    printf("||==========================================||\n");
+    printf("||-----------SELECCIONE-ALGORITMO-----------||\n");
+    printf("|| 1. FIFO                                  ||\n");
+    printf("|| 2. SJF                                   ||\n");
+    printf("|| 3. HPF                                   ||\n");
+    printf("|| 4. ROUND ROBIN                           ||\n");
+    printf("|| 5. SALIR                                 ||\n");
+    printf("||==========================================||\n");
+    printf("||--------INTRODUZCA-UNA-OPCION-(1-4)-------||\n");
+    printf("||Opcion: ");
+    scanf("%d", &opcion);
+
+    switch (opcion)
+    {
+    case 1:
+        /* code */
+        pthread_create(&menuThread, NULL, &menuServer, NULL);
+        connection();
+        break;
+
+    case 2:
+        /* code */
+        pthread_create(&menuThread, NULL, &menuServer, NULL);
+        connection();
+        break;
+
+    case 3:
+        /* code */
+        pthread_create(&menuThread, NULL, &menuServer, NULL);
+        connection();
+        break;
+
+    case 4:
+        /* code */
+        pthread_create(&menuThread, NULL, &menuServer, NULL);
+        connection();
+        break;
+
+    case 5:
+        /* code */
+        pthread_create(&menuThread, NULL, &menuServer, NULL);
+        connection();
+        break;
+
+    default:
+        break;
+    }
+
     return 0;
 }
--- Server/connection.c
+++ Server/connection.c
@@ -10,10 +10,92 @@
 
 pthread_t jobScheduler;
 int GlobalID = 0;
+struct node *headTaskList = NULL;
+// struct node *headThreads = NULL;
+
+
+struct arguments
+{
+    int id;
+    char *name;
+    int priority;
+    int burst;
+};
+
+struct node
+{
+    int id;
+    int burst;
+    int priority;
+    struct node *next;
+};
+
+int insert(int pID, int pPrority, int pBurst)
+{
+    //create a link
+    struct node *link = (struct node *)malloc(sizeof(struct node));
+    struct node *current = (struct node *)malloc(sizeof(struct node));
+
+    //link->key = key;
+    link->id = pID;
+    link->priority = pPrority;
+    link->burst = pBurst;
+
+    //point it to old first node
+    if (headTaskList == NULL)
+    {
+        headTaskList = link;
+        return 1;
+    }
+    current = headTaskList;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = link;
+    link = NULL;
+    current = NULL;
+    free(link);
+    free(current);
+    return 1;
+}
+
+void printJobTaskList()
+{
+    struct node *current = (struct node *)malloc(sizeof(struct node));
+    current = headTaskList;
+    if (current == NULL)
+    {
+        printf("||Cola de tareas: ");
+        printf("Lista Vacia");
+    }
+    while (current != NULL)
+    {
+        printf("Burst: %d", current->burst);
+        printf("->");
+        current = current->next;
+    }
+    printf("\n");
+}
+
+struct node *getFirstRM()
+{
+    struct node *current = (struct node *)malloc(sizeof(struct node));
+    current = headTaskList;
+    headTaskList = headTaskList->next;
+    return current;
+}
+
 
 /////////////////////////////////////////////////////
 //                 CREA UN JSON                    //
 /////////////////////////////////////////////////////
+
+void connectionPrintJobTaskList()
+{
+    printJobTaskList(headTaskList);
+}
+
 json_object *makeJson(int pData1)
 {
     /*Creating a json object*/
@@ -27,19 +109,11 @@ json_object *makeJson(int pData1)
     return jobj;
 }
 
-struct arguments
-{
-    int id;
-    char *name;
-    int priority;
-    int burst;
-};
 
 void *jobSchedulerTask(void *pArgs)
 {
     struct arguments *args = (struct arguments *)pArgs;
     insert(args->id, args->priority, args->burst);
-    printJobTaskList();
 }
 
 void *connection_handler(void *socket_desc)
@@ -76,9 +150,9 @@ void *connection_handler(void *socket_desc)
         burstT = json_object_get_int(burst);
         priorityT = json_object_get_int(priority);
 
-        printf("Name: %c\n", *nameT);
-        printf("BURST: %d\n", burstT);
-        printf("PRIORITY: %d\n", priorityT);
+        // printf("Name: %c\n", *nameT);
+        // printf("BURST: %d\n", burstT);
+        // printf("PRIORITY: %d\n", priorityT);
 
         struct arguments *args = (struct arguments *)malloc(sizeof(struct arguments));
         GlobalID = GlobalID + 1;
@@ -87,12 +161,11 @@ void *connection_handler(void *socket_desc)
         args->priority = priorityT;
         args->burst = burstT;
 
-        puts("EL CLIENTE ENVIO :");
-        puts(client_message);
+        // puts("EL CLIENTE ENVIO :");
+        // puts(client_message);
 
         pthread_create(&jobScheduler, NULL, &jobSchedulerTask, (void *)args);
         jobj = makeJson(args->id);
-
 
         if (strcpy(temp_buff, json_object_to_json_string(jobj)) == NULL)
         {
@@ -101,7 +174,7 @@ void *connection_handler(void *socket_desc)
 
         //ACA LO ENVIA
         //send(sock, message, strlen(message), 0) < 0
-        printf("|| ESPERA 2 SEGUNDOS ANTES HACER EL ENVIO : ||\n");
+        // printf("|| ESPERA 2 SEGUNDOS ANTES HACER EL ENVIO : ||\n");
         sleep(2);
         if (send(sock, temp_buff, strlen(temp_buff), 0) < 0)
         {
@@ -135,7 +208,7 @@ int connection()
     {
         printf("Could not create socket");
     }
-    puts("Socket created");
+    // puts("Socket created");
 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
@@ -146,16 +219,16 @@ int connection()
         perror("bind failed. Error");
         return 1;
     }
-    puts("bind done");
+    // puts("bind done");
 
     listen(socket_desc, 3);
 
-    puts("Waiting for incoming connections...");
+    // puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
 
     while ((client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c)))
     {
-        puts("Connection accepted");
+        // puts("Connection accepted");
 
         pthread_t sniffer_thread;
         new_sock = malloc(1);
@@ -167,7 +240,7 @@ int connection()
             return 1;
         }
 
-        puts("Handler assigned");
+        // puts("Handler assigned");
     }
 
     if (client_sock < 0)
